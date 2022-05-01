2bee2f2e5fbb0516295702ed417cdc919851dfd7
Unclemitsamay <mitsamay2019@gmail.com>
2022-05-01 17:26:43 +0700

    update

--- school/urls.py
+++ school/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, AboutPage, ContactPage
 
 urlpatterns = [
     #localhost:8000/
-    path('', HomePage)
+    path('', HomePage, name="home-page"),
+    #localhost:8000/about
+    path('about/', AboutPage, name='about-page'),
+    #localhost:8000/contact
+    path('contact/', ContactPage, name='contact-page')
 ]
--- school/views.py
+++ school/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render  # ດຶງມາຈາກ template
+from django.http import HttpResponse  # ຂຽນເທິງກະດານ
 
 # Create your views here.
 def HomePage(request) :
-    return HttpResponse('<h1>Home Page</h1>')
+     # return HttpResponse('<h1>Home Page</h1>')
+     return render(request, 'school/home.html')
 
-#def about(request) :
- #   return HttpResponse('About Page')
+def AboutPage(request) :
+     #return HttpResponse('About Page')
+     return render(request, 'school/about.html')
 
-#def contact(request) :
- #   return HttpResponse('Contact Page')
+def ContactPage(request) :
+     #return HttpResponse('Contact Page')
+     return render(request, 'school/contact.html')
 
 #def persons(request) :
  #   return HttpResponse('Persons Page')
